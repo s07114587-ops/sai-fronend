fcb04e828c4772dd676cca093ebaf2f595f81157
s07114587-ops <s07114587@gmail.com>
2026-06-19 22:24:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// ওয়েলকাম স্ক্রিন ক্লোজ করার ফাংশন
+function closeWelcomeScreen() {
+    const welcomeScreen = document.getElementById('welcome-screen');
+    if (welcomeScreen) {
+        welcomeScreen.style.display = 'none';
+    }
+}
